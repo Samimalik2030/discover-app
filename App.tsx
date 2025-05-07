b58335b6fbba3ede55611c1fb04228c397913c90
Samimalik2030 <msamiullah2030@gmail.com>
2025-05-07 17:05:22 +0500

    some fixes

--- App.tsx
+++ App.tsx
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.tsx
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Provider as PaperProvider } from "react-native-paper";
+import Home from "./src/Home";
+import AreaPrefernce from "./src/AreaPreference";
+import VacationType from "./src/VacationType";
+import DestinationCard from "./src/DestinationCard";
+import DestinationDetail from "./src/DestinationDetail";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="AreaPrefernce"
+            component={AreaPrefernce}
+            options={{
+              headerShown: true,
+            }}
+          />
+          <Stack.Screen
+            name="VacationType"
+            component={VacationType}
+            options={{
+              headerShown: true,
+            }}
+          />
+          <Stack.Screen
+            name="DestinationCard"
+            component={DestinationCard}
+            options={{
+              headerShown: true,
+            }}
+          />
+          <Stack.Screen
+            name="DestinationDetail"
+            component={DestinationDetail}
+            options={{
+              headerShown: true,
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
